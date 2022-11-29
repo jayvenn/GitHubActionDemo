0b8ad15fedbbe8dfed2dff7d36a7066af7781688
Jayven Nhan <nhantrivinh@gmail.com>
2022-11-29 10:06:12 -0400

    Initial commit

---
+++ GitHubActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionDemoApp.swift
+//  GitHubActionDemo
+//
+//  Created by MyMac on 2022-11-29.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
